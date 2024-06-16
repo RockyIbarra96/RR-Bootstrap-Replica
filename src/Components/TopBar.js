b7d16be839520e3a7ee94879890d167783bbb9d3
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-06-16 14:53:53 -0700

    added styling to topbar

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,27 +1,36 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function TopBar() {
     return (
-        <div>
-            <Navbar bg="light" expand="lg">
-                <Container className="d-flex justify-content-between align-items-center">
-                    <Navbar.Brand href="#home">
-                        <img
-                            src="https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg"
-                            alt="instacart-logo"
-                            style={{ height: '40px' }}
-                        />
-                    </Navbar.Brand>
-                    <Nav className="d-flex align-items-center">
-                        <Nav.Link href="#LogIn" style={{ marginRight: '10px', color: "#343538", fontSize: '18px', fontWeight: 'bolder' }}>Log In</Nav.Link>
-                        <Button variant="success" style={{ fontSize: '18px' }}>Sign Up</Button>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Row>
+                    {/* First Column for Logo */}
+                    <Col md={4}>
+                        <Navbar.Brand href="#home">
+                            <img
+                                src="https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg"
+                                alt="instacart-logo"
+                                style={{ height: '40px' }}
+                            />
+                        </Navbar.Brand>
+                    </Col>
+                    
+                    {/* Second Column for Login and Signup */}
+                    <Col md={8}>
+                        <Nav>
+                            <Nav.Link href="#LogIn" style={{ marginRight: '10px', color: "#343538", fontSize: '18px', fontWeight: 'bolder' }}>Log In</Nav.Link>
+                            <Button variant="success" style={{ fontSize: '18px', background: "lightgreen" }}>Sign Up</Button>
+                        </Nav>
+                    </Col>
+                </Row>
+            </Container>
+        </Navbar>
     );
 }
-
